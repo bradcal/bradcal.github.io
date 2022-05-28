9b80ca8f9adce1d927f36fb6072a9c678d8dc243
Caleb Brady <47161975+bradcal@users.noreply.github.com>
2022-05-28 14:31:19 -0700

    started caleb's js

---
+++ cable1.js
@@ -0,0 +1,5 @@
+function revealAboutMe() {
+    document.getElementById("AboutMe").innerHTML = "Click the button again to see the pop-up about me another time!";
+    alert("I am a student at UWB studying Interactive Media Design. This web design class is the only class I am taking this quarter, but I am also working and volunteering throughout my week!");
+
+}

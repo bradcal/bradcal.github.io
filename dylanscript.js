cbae41fc6974eb71a49c0baac33925c2103cb437
DylanNDang <82989695+DylanDangUW@users.noreply.github.com>
2022-05-28 15:32:13 -0700

    Update dylanscript.js

--- dylanscript.js
+++ dylanscript.js
@@ -2,3 +2,18 @@ function press() {
     var p = document.getElementById("paragraph");
     p.style.display = "block";  	 
   }
+  (() => {
+    const h1El = document.querySelector('h1');
+    const letters = h1El.textContent.split('');
+    h1El.innerHTML = letters.map((l, i) => `<p style="--i: ${i}; --j: ${letters.length -1 - i}">${l == ' ' ? '&nbsp;': l}</p>`).join('');
+    
+    h1El.addEventListener('mouseenter', () => {
+      h1El.classList.add('flip-in');
+      h1El.classList.remove('flip-out');
+    });
+    
+    h1El.addEventListener('mouseleave', () => {
+      h1El.classList.remove('flip-in');
+      h1El.classList.add('flip-out');
+    });
+  })()

d7aed2a5a0bfbc8f9ab39f349bd6e3c83d5f3d6e
Caleb Brady <47161975+bradcal@users.noreply.github.com>
2022-06-01 17:31:41 -0700

    caleb finishing page

--- cable1.js
+++ cable1.js
@@ -3,3 +3,25 @@ function revealAboutMe() {
     alert("I am a student at UWB studying Interactive Media Design. This web design class is the only class I am taking this quarter, but I am also working and volunteering throughout my week!");
 
 }
+
+
+let counter = 0;
+function countUp() {
+    if (counter < 10) {
+        counter++;
+        document.getElementById("counter").innerHTML += counter;
+        console.log(counter);
+    } else {
+        document.getElementById("counter").innerHTML = "you win!";
+    }
+    return;
+}
+
+let food = ['cheese ', 'bacon ', 'chicken strips ', 'hamburgers ', 'pasta ', 'broccoli ', 'brussel sprouts ', 'sandwiches ', 'corn ', 'pizza ', 'eggs ']
+function pickFood() {
+    var item = food[Math.floor(Math.random()*food.length)];
+    while (item != 'cheese ') {
+        var item = food[Math.floor(Math.random()*food.length)];
+        document.getElementById("picker").innerHTML += item;
+    }
+}

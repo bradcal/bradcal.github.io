2ecaf3dba77b44ca1ff865234114968f2e77d65a
PhuongPhan95 <phanp95@uw.edu>
2022-06-02 23:48:58 -0700

    Update comments

--- Piper.js
+++ Piper.js
@@ -1,8 +1,11 @@
+//set a variables
 let counter = 0;
 let drink = [' Happy lemon', ' TP Tea', ' YiFang', ' Seattle Best Tea', ' Daboba']
 
-function countUp() {
+//write a function
+function listItem() {
     if (counter < 5) {
+        //print out item
         document.getElementById("counter").innerHTML += drink[counter] + ",";
         counter++;
     } else {

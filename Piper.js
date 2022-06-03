7b3d04d87dfe1961564603a41027a510e1c144ba
PhuongPhan95 <phanp95@uw.edu>
2022-06-02 23:39:05 -0700

    Update JS

--- Piper.js
+++ Piper.js
@@ -3,7 +3,7 @@ let drink = [' Happy lemon', ' TP Tea', ' YiFang', ' Seattle Best Tea', ' Daboba
 
 function countUp() {
     if (counter < 5) {
-        document.getElementById("counter").innerHTML += drink[counter];
+        document.getElementById("counter").innerHTML += drink[counter] + ",";
         counter++;
     } else {
         document.getElementById("counter").innerHTML = "It is all!";
